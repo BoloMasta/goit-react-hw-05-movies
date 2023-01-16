7ed9ee58dd026a719d635f1595a1b50f058621fa
Bolek <103572991+BoloMasta@users.noreply.github.com>
2023-01-16 13:11:09 +0100

    browserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { App } from 'components/App';
 // import './index.css';
@@ -8,9 +8,9 @@ import { GlobalStyles } from 'Styled/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/index.html">
+    <Router basename="/index.html">
       <GlobalStyles />
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
