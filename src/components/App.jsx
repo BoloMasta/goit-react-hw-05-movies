b9d58ff9ff2af1bdc05112dfac83ecb233ae0718
BoloMasta <boleslawadamiec@gmail.com>
2023-01-16 21:50:14 +0100

    fix2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 
 import { SharedLayout } from './SharedLayouts';
+import MovieDetails from '../pages/MovieDetails';
 
 const Home = lazy(() => import('../pages/Home'));
-const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+// const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('../components/Cast'));
 const Reviews = lazy(() => import('../components/Reviews'));
 const Movies = lazy(() => import('../pages/Movies'));
