7d50ed7510abe8ed109d97ea549585119cf969e6
BoloMasta <boleslawadamiec@gmail.com>
2023-01-17 22:01:48 +0100

    swr for cast

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect, useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 import { Wrapper, List, Item, Image, Name, Character } from '../Styled/Cast';
-import api from 'services/api';
+import { useRequest } from '../services/useRequest';
+// import api from 'services/api';
 
 const castReducer = (state, action) => {
   switch (action.type) {
@@ -24,27 +25,17 @@ const castReducer = (state, action) => {
 const Cast = () => {
   const { movieId } = useParams();
   const [cast, dispatch] = useReducer(castReducer, []);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const { data, error } = useRequest(`/movie/${movieId}/credits`);
 
   useEffect(() => {
-    setIsLoading(true);
-    const getCast = async () => {
-      try {
-        const { cast } = await api.fetchMovieCast(movieId);
-        const castWithLoadedProp = cast.map(castMember => ({
-          ...castMember,
-          loaded: false,
-        }));
-        dispatch({ type: 'SET_CAST', payload: castWithLoadedProp });
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getCast();
-  }, [movieId]);
+    if (data) {
+      const castWithLoadedProp = data.cast.map(castMember => ({
+        ...castMember,
+        loaded: false,
+      }));
+      dispatch({ type: 'SET_CAST', payload: castWithLoadedProp });
+    }
+  }, [data]);
 
   const handleCastImageLoaded = e => {
     const castMemberId = Number(e.target.dataset.id);
@@ -54,7 +45,7 @@ const Cast = () => {
   return (
     <>
       {error && <h2>Something went wrong. Try again later.</h2>}
-      {isLoading && (
+      {!data ? (
         <Wrapper>
           <List>
             <Item>
@@ -68,11 +59,9 @@ const Cast = () => {
             </Item>
           </List>
         </Wrapper>
-      )}
-
-      {!isLoading && !error && (
+      ) : (
         <>
-          {cast.length === 0 && !isLoading ? (
+          {cast.length === 0 && !data ? (
             <h2>We don't have any cast for this movie.</h2>
           ) : (
             <Wrapper>
@@ -84,7 +73,7 @@ const Cast = () => {
                       src={
                         profile_path
                           ? `https://image.tmdb.org/t/p/w500${profile_path}`
-                          : 'https://dummyimage.com/200x300/2a2a2a/ffffff&text=No+image'
+                          : 'https://via.placeholder.com/150'
                       }
                       alt={name}
                       data-id={id}
@@ -93,13 +82,10 @@ const Cast = () => {
                       width={loaded ? 150 : 0}
                     />
                     <Name>{name}</Name>
-                    <Character>Character: {character}</Character>
+                    <Character>{character}</Character>
                   </Item>
                 ))}
               </List>
-              {cast.length === 0 && !isLoading && (
-                <h2>We don't have any cast for this movie.</h2>
-              )}
             </Wrapper>
           )}
         </>
@@ -107,5 +93,88 @@ const Cast = () => {
     </>
   );
 };
+
+// const [error, setError] = useState(null);
+// const [isLoading, setIsLoading] = useState(false);
+
+// useEffect(() => {
+//   setIsLoading(true);
+//   const getCast = async () => {
+//     try {
+//       const { cast } = await api.fetchMovieCast(movieId);
+//       const castWithLoadedProp = cast.map(castMember => ({
+//         ...castMember,
+//         loaded: false,
+//       }));
+//       dispatch({ type: 'SET_CAST', payload: castWithLoadedProp });
+//     } catch (error) {
+//       setError(error);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+//   getCast();
+// }, [movieId]);
+
+// const handleCastImageLoaded = e => {
+//   const castMemberId = Number(e.target.dataset.id);
+//   dispatch({ type: 'SET_LOADED', payload: castMemberId });
+// };
+
+// return (
+//   <>
+//     {error && <h2>Something went wrong. Try again later.</h2>}
+//     {isLoading && (
+//       <Wrapper>
+//         <List>
+//           <Item>
+//             <Skeleton width={150} height={200} inline={true} />
+//             <Name>
+//               <Skeleton width={200} height={50} inline={true} />
+//             </Name>
+//             <Character>
+//               <Skeleton width={400} height={50} inline={true} />
+//             </Character>
+//           </Item>
+//         </List>
+//       </Wrapper>
+//     )}
+
+//     {!isLoading && !error && (
+//       <>
+//         {cast.length === 0 && !isLoading ? (
+//           <h2>We don't have any cast for this movie.</h2>
+//         ) : (
+//           <Wrapper>
+//             <List>
+//               {cast.map(({ id, profile_path, name, character, loaded }) => (
+//                 <Item key={id}>
+//                   {!loaded && <Skeleton width={150} height={195} />}
+//                   <Image
+//                     src={
+//                       profile_path
+//                         ? `https://image.tmdb.org/t/p/w500${profile_path}`
+//                         : 'https://dummyimage.com/200x300/2a2a2a/ffffff&text=No+image'
+//                     }
+//                     alt={name}
+//                     data-id={id}
+//                     onLoad={handleCastImageLoaded}
+//                     height={loaded ? 200 : 0}
+//                     width={loaded ? 150 : 0}
+//                   />
+//                   <Name>{name}</Name>
+//                   <Character>Character: {character}</Character>
+//                 </Item>
+//               ))}
+//             </List>
+//             {cast.length === 0 && !isLoading && (
+//               <h2>We don't have any cast for this movie.</h2>
+//             )}
+//           </Wrapper>
+//         )}
+//       </>
+//     )}
+//   </>
+// );
 
 export default Cast;
