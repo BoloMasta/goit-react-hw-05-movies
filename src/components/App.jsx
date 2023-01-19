128c46cea59b18aa72e233852fce87f17223aeb6
Bolek <103572991+BoloMasta@users.noreply.github.com>
2023-01-19 17:48:54 +0100

    lazy removed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 
 import { SharedLayout } from './SharedLayouts';
-// import MovieDetails from '../pages/MovieDetails';
-// import Cast from '../components/Cast';
-// import Reviews from '../components/Reviews';
-// import NotFound from '../pages/NotFound';
+import MovieDetails from '../pages/MovieDetails';
+import Cast from '../components/Cast';
+import Reviews from '../components/Reviews';
+import NotFound from '../pages/NotFound';
 
 const Home = lazy(() => import('../pages/Home'));
-const MovieDetails = lazy(() => import('../pages/MovieDetails'));
-const Cast = lazy(() => import('../components/Cast'));
-const Reviews = lazy(() => import('../components/Reviews'));
+// const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+// const Cast = lazy(() => import('../components/Cast'));
+// const Reviews = lazy(() => import('../components/Reviews'));
 const Movies = lazy(() => import('../pages/Movies'));
-const NotFound = lazy(() => import('../pages/NotFound'));
+// const NotFound = lazy(() => import('../pages/NotFound'));
 
 export const App = () => {
   return (
