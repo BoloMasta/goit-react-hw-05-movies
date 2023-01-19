b6a90bafa3df53186802bd4d16a64d247970d4f0
BoloMasta <boleslawadamiec@gmail.com>
2023-01-19 22:43:22 +0100

    #

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,7 @@ const NotFound = lazy(() => import('../pages/NotFound'));
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<SharedLayout />}>
+      <Route path="#/" element={<SharedLayout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<MovieDetails />}>
