975b148237751f12c12001de07f1c7deb3dab6dc
Bolek <103572991+BoloMasta@users.noreply.github.com>
2023-01-20 10:44:51 +0100

    gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SWRConfig } from 'swr';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import { GlobalStyles } from 'Styled/GlobalStyles';
 
@@ -9,11 +9,11 @@ const fetcher = (...args) => fetch(...args).then(response => response.json());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/">
       <SWRConfig value={{ fetcher }}>
         <GlobalStyles />
         <App />
       </SWRConfig>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
